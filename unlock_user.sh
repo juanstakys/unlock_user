d4512c369f9a6d9c82dc8d5047867c625c7747ac
Juan Cruz Stakys <52844841+juanstakys@users.noreply.github.com>
2024-04-30 21:44:59 -0300

    Fix

--- unlock_user.sh
+++ unlock_user.sh
@@ -3,11 +3,12 @@
 #$1 representa el dispositivo donde se en encuentra el usuario
 #$2 representa el nombre de usuario
 
-if [[ -z $1 && -z $2 ]]; then
-	mount $1 /mnt &&
+if [[ -z $1 || -z $2 ]]; then
+	echo "An error ocurred"
+ 	exit 1
+else
+ 	mount $1 /mnt &&
 	passwd --root /mnt $2 &&
 	umount /mnt
 	echo "Done! You can reboot now"
-else
-	echo "An error ocurred"
 fi
